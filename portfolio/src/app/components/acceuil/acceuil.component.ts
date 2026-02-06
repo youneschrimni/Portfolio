a015469f1cea70c568f080c3701c2a5d40196491
Younes Chrimni <chrimniyounes@gmail.com>
2026-02-06 22:44:27 +0100

    fix import missing

--- portfolio/src/app/components/acceuil/acceuil.component.ts
+++ portfolio/src/app/components/acceuil/acceuil.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-acceuil',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './acceuil.component.html',
   styleUrls: ['./acceuil.component.css']
 })
